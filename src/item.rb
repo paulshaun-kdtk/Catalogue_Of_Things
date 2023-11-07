6fa7d0b895356bccc7dd10185989577d23b9b762
Omwomo <eugeneoduor79@gmail.com>
2023-11-07 09:23:19 +0300

    create Item class with attributes and methods

---
+++ src/item.rb
@@ -0,0 +1,21 @@
+class Items
+  attr_accessor :genre, :author, :source, :label
+
+  def initialize(publish_date, archived: true)
+    @id = Random.rand(1..1000)
+    @publish_date = publish_date
+    @archived = true
+  end
+
+  private
+
+  def can_be_archived?
+    return unless @publish_date > 10
+  end
+
+  public
+
+  def move_to_archive
+    can_be_archived?
+  end
+end
