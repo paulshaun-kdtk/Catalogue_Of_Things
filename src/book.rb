19f42c8fbff5a8af896e372f682aaf7d9a05d622
Omwomo <eugeneoduor79@gmail.com>
2023-11-07 22:45:54 +0300

    Implemented the add item method in the label class

--- src/book.rb
+++ src/book.rb
@@ -1,5 +1,5 @@
 class Book < Items
-  def initialize(publisher, cover_state)
+  def initialize(publish_date, publisher, cover_state)
     super(publish_date)
     @publisher = publisher
     @cover_state = cover_state
