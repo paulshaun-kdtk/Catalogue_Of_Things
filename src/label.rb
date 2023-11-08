627d955e54cf634031220e5fed8013b6acf5ae0b
Omwomo <eugeneoduor79@gmail.com>
2023-11-08 20:20:12 +0300

    Preserved all label data in json file

--- src/label.rb
+++ src/label.rb
@@ -1,5 +1,7 @@
+require 'json'
+
 class Label
-  attr_accessor :items
+  attr_accessor :items, :title, :color
 
   def initialize(title, color)
     @id = Random.rand(1..1000)
@@ -13,8 +15,32 @@ class Label
     item.label = self
   end
 
-  def label=(label)
-    @label = label
-    label.items.push(self) unless label.items.include?(self)
+  # Serializing data to json
+  def label_to_json
+    {
+      title: @title,
+      color: @color
+    }
   end
+
+  # Deserializing data from json
+  def self.from_json(json_hash)
+    new(json_hash['title'], json_hash['color'])
+  end
+end
+
+def save_labels(labels)
+  File.write('storage/labels.json', JSON.generate(labels.map(&:label_to_json)))
+end
+
+def load_labels
+  loaded_labels = []
+  if File.exist?('storage/labels.json')
+    data = JSON.parse(File.read('storage/labels.json'))
+    data.each do |label_data|
+      loaded_labels << Label.from_json(label_data)
+    end
+  end
+
+  loaded_labels
 end
